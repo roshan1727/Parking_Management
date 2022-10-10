c39b9807d4c2b2e0c213d0ea0326dfafa0a65739
roshan1727 <rakeshroshanp27@gmail.com>
2022-10-11 01:57:08 +0530

    Initial commit

---
+++ src/Bike.java
@@ -0,0 +1,27 @@
+public class Bike extends Vehicle {
+
+    private boolean suspension;
+
+    public Bike(){
+        super();
+    }
+
+    public Bike(String model, String color, int numOfWheels, boolean suspension){
+        super( model, color, numOfWheels, "na"); // na for transmission as Bike class does not need a suspension property
+        this.suspension = suspension;
+    }
+    //getter
+    public boolean getSuspension(){
+        return suspension;
+    }
+    //setter
+    public void setSuspension(){
+        this.suspension = suspension;
+    }
+
+    //method
+    public void printBikeDetails(){
+        super.printDetails();
+        System.out.println("Does this bike have suspension ?? " + this.suspension);
+    }
+}
---
+++ src/Truck.java
@@ -0,0 +1,32 @@
+public class Truck extends Vehicle{
+
+    private int numOfContainers;
+
+    public Truck(){
+        super();
+    }
+
+    public Truck(String model, String color, int numOfWheels, String transmission, int numOfContainers){
+        super ( model, color, numOfWheels, transmission) ;
+        this.numOfContainers = numOfContainers;
+    }
+
+    //getters
+
+    public int getnumOfContainers(){
+        return numOfContainers;
+    }
+
+    //setter
+    public void setnumOfContainers(){
+        this.numOfContainers = numOfContainers;
+    }
+
+    //Method
+    public void printTruckdetails(){
+        super.printDetails();
+        System.out.println(" Number of Containers in this Truck are: " + this.numOfContainers);
+
+    }
+
+}
